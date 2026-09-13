588924a90dffb5d57a001477b34d7d1088072992
hxcy521-cpu <hxcy521@gmail.com>
2026-09-13 16:07:32 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='big-man-d3-cue-2026-09-13-9';
+const CORE=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-512.png','./version.json'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{const request=event.request;if(request.method!=='GET')return;const url=new URL(request.url);if(url.origin!==self.location.origin)return;event.respondWith(fetch(request).then(response=>{if(response&&response.ok){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(request,copy))}return response}).catch(()=>caches.match(request).then(hit=>hit||(request.mode==='navigate'?caches.match('./index.html'):Response.error()))))});
